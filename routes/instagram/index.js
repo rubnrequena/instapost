0cd93ee71c7a884aef2c3b39e30d7df37d8eef07
Ruben Requena <rubnrequena@gmail.com>
2021-02-19 08:01:08 +0100

    retornar error

--- routes/instagram/index.js
+++ routes/instagram/index.js
@@ -31,7 +31,8 @@ module.exports = async function (fastify, opts) {
       igm.post(texto, fileUrl)
         .then(({ media }) => reply.send(media))
         .catch(error => {
-          console.error('ERROR:', error);
+          reply.send(error.error)
+          console.error('ERROR:', error.error);
         })
     })
   })
